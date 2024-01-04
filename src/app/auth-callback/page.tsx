67584acab3c23942a05bb9dc186d7db3e22cdf76
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-01-04 11:00:31 +0200

    feat: connect db

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 import { useRouter, useSearchParams } from "next/navigation";
+import { trpc } from "../_trpc/client";
 
 const AuthCallback = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
 
-  
+  const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+    onSuccess: ({ success }) => {
+      if (success) {
+        router.push(origin ? `/${origin}` : "/dashboard");
+      }
+    },
+  });
 
   return null;
 };
