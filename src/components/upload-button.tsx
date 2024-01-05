697a0560c0e5c46ebcd80403665076e52108d1ee
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-01-05 10:15:02 +0200

    feat: add files list & dialog

---
+++ src/components/upload-button.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useState } from "react";
+import { Button, buttonVariants } from "./ui/button";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+const UploadButton = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger className={buttonVariants()}>Upload PDF</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default UploadButton;
