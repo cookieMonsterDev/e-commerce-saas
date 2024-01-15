a3c95eedc591cd5919b40de8b31c5a19b4a23eb4
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-01-15 12:13:43 +0200

    feat: add losts of stuff

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -13,7 +13,7 @@ const Dashboard = () => {
     string | null
   >(null);
 
-  const utils = trpc.useContext();
+  const utils = trpc.useUtils();
 
   const { data: files, isLoading } = trpc.getUserFiles.useQuery();
 
@@ -49,10 +49,10 @@ const Dashboard = () => {
               .map((file) => (
                 <li
                   key={file.id}
-                  className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow hover:shadow-lg"
+                  className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg"
                 >
                   <Link
-                    href={`/dahsboard/${file.id}`}
+                    href={`/dashboard/${file.id}`}
                     className="flex flex-col gap-2"
                   >
                     <div className="pt-6 px-6 flex w-full items-center justify-between space-x-6">
@@ -65,32 +65,32 @@ const Dashboard = () => {
                         </div>
                       </div>
                     </div>
-
-                    <div className="px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-500">
-                      <div className="flex items-center gap-2">
-                        <Plus className="h-4 w-4" />
-                        {format(new Date(file.createdAt), "MMM yyyy")}
-                      </div>
-
-                      <div className="flex items-center gap-2">
-                        <MessageSquare className="h-4 w-4" />
-                        mocked
-                      </div>
-
-                      <Button
-                        onClick={() => deleteFile({ id: file.id })}
-                        size="sm"
-                        className="w-full"
-                        variant="destructive"
-                      >
-                        {currentlyDeletingFile === file.id ? (
-                          <Loader2 className="h-4 w-4 animate-spin" />
-                        ) : (
-                          <Trash className="h-4 w-4" />
-                        )}
-                      </Button>
-                    </div>
                   </Link>
+
+                  <div className="px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-500">
+                    <div className="flex items-center gap-2">
+                      <Plus className="h-4 w-4" />
+                      {format(new Date(file.createdAt), "MMM yyyy")}
+                    </div>
+
+                    <div className="flex items-center gap-2">
+                      <MessageSquare className="h-4 w-4" />
+                      mocked
+                    </div>
+
+                    <Button
+                      onClick={() => deleteFile({ id: file.id })}
+                      size="sm"
+                      className="w-full"
+                      variant="destructive"
+                    >
+                      {currentlyDeletingFile === file.id ? (
+                        <Loader2 className="h-4 w-4 animate-spin" />
+                      ) : (
+                        <Trash className="h-4 w-4" />
+                      )}
+                    </Button>
+                  </div>
                 </li>
               ))}
           </ul>
